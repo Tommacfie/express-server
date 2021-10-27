e6f4b4f23be573276ec81f734c708a07c423d4e3
Tommacfie <tommacfie@aol.com>
2021-10-27 14:30:07 +0200

    feat: add router middleware and server listen method
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const app = Express();
 const router = require('./router');
 
 const dotenv = require('dotenv').config();
+
+app.use(router);
+
+app.listen(PORT, (error) => {
+  if (error) console.log('error');
+  console.log(`Server is running on ${PORT}`);
+})
