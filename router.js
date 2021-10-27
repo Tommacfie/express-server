4c515bd2a2ead75647920d6c2a5ccdb506267b66
Tommacfie <tommacfie@aol.com>
2021-10-27 14:37:24 +0200

    feat: implement router PUT method
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.get('/', () => console.log('Router GET'));
 router.post('/', () => console.log('Router POST'));
+router.put('/', () => console.log('Router PUT'));
 router.delete('/', () => console.log('Router DELETE'));
 
 module.exports = router;
