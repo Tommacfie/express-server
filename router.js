5ef9dbb41db69e31da58928df9b11ab4c80b0dab
Tommacfie <tommacfie@aol.com>
2021-10-27 14:34:59 +0200

    feat: implement router DELETE method
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- router.js
+++ router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.get('/', () => console.log('Router GET'));
 router.post('/', () => console.log('Router POST'));
+router.delete('/', () => console.log('Router DELETE'));
 
 module.exports = router;
