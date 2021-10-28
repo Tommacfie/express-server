97c4bc6a74bb139ecc2a0b7f25d6cadc54a0ca3b
Tommacfie <tommacfie@aol.com>
2021-10-28 15:24:38 +0200

    feat: add default options to avoid warnings mongoose connection
    Signed-off-by: Tommacfie <tommacfie@aol.com>

--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const { DATABASE_URL } = require('../config');
 
-mongoose.connect(DATABASE_URL, (error) => {
-  if (error) console.log('Error connecting to database server');
-  console.log(`Mongodb server running on ${DATABASE_URL}`);
-})
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},
+  (error) => {
+    if (error) console.log('Error connecting to database server');
+    console.log(`Mongodb server running on ${DATABASE_URL}`);
+  })
 
 module.exports = mongoose;
