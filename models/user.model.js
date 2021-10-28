44fe71fa2433ec119d41982db0a53da6db10a15e
Tommacfie <tommacfie@aol.com>
2021-10-28 15:21:16 +0200

    feat: create user model
    Signed-off-by: Tommacfie <tommacfie@aol.com>

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('./index');
+
+const Schema = mongoose.Schema;
+
+const userModel = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', userModel);
+
+module.exports = User;
