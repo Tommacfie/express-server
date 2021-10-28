03ca7978adbb50ad7c71a257e37739fd2e3856f3
Tommacfie <tommacfie@aol.com>
2021-10-28 15:18:08 +0200

    feat: create topic model
    Signed-off-by: Tommacfie <tommacfie@aol.com>

---
+++ models/topic.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('./index');
+
+const Schema = mongoose.Schema();
+
+const topicModel = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true
+  }
+});
+
+const Topic = mongoose.model('Topic', topicModel);
+
+module.exports = Topic;
